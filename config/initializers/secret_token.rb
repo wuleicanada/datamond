86d3b1a3a6e4651dc2fed320ce966e4b500bede2
lwu <lwu@marketwire.com>
2013-10-04 10:04:43 -0400

    init blank rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Datamond::Application.config.secret_key_base = '9fe9b03340904aab942b25c3747e69c4b97ac7bfb0440d1f02320d7c1224a81d4642ce7fced9fcc4db0a1fc3615b26c785316d72540961d7a813931d1695a16b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Datamond::Application.config.session_store :cookie_store, key: '_datamond_session'
