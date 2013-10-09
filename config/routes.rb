1a18af3be2028bf42f4645f7a0c440c599f5eae1
lwu <lwu@marketwire.com>
2013-10-09 11:12:46 -0400

    about.htm

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Datamond::Application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  
+  get 'about.htm' => 'about#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
