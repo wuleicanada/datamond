1acebe0ef9485203666ece914bdac094d1937d50
lwu <lwu@marketwire.com>
2013-10-09 11:28:07 -0400

    team.htm

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Datamond::Application.routes.draw do
   get 'index.htm' => 'welcome#index'
   get 'about.htm' => 'about#index'
   get 'contact.htm' => 'contact#index'
+  get 'team.htm' => 'team#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
