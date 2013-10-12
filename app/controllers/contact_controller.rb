09a5b62ae7ff1110374dfe32cccdb5ffea47797b
lwu <lwu@marketwire.com>
2013-10-11 20:03:24 -0400

    contact using form_tag

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,5 +1,20 @@
 class ContactController < ApplicationController
-  def index
+  def new
     @page_name = 'contact'
+    @contact = Contact.new
+  end
+  
+  def create
+    @contact = Post.new(post_params)
+    @contact.save
+    redirect_to @contact
+  end  
+  
+  def index
+    render :new
+  end
+
+  def show
+    @contact = Contact.find(params[:id])	  
   end
 end
